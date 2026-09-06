504afda828217f962faf18942ada468eb4f93fcb
schoolonlyaccount <macman5@tkf7.net>
2026-09-05 22:00:29 -0500

    Background Color Tweak

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,8 +53,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full bg-black antialiased`}>
+      <body className="min-h-full bg-black flex flex-col">
         <Header />
 
         {children}
